579c03304a8c8b3ff2f8640f61c3c2e2fcfb1a4b
Sol-III-Observer <12497667+sol-iii-observer@user.noreply.gitee.com>
2024-05-31 08:46:47 +0800

    feat: Improve rectangle printer in explorer.py

--- explorer.py
+++ explorer.py
@@ -29,7 +29,7 @@ class tree(AbstractPrinter):
         for i in pre_list:
             pre += i
         n = len(data.keys())
-        pre_list.append("|")
+        pre_list.append("│")
         for k in data.keys():
             line = ""
             if n == 1:
@@ -46,7 +46,7 @@ class tree(AbstractPrinter):
                 line += self.icon["leaf"]
                 line += k
                 if data[k] != None:
-                    line += ':' + data[k]
+                    line += ": " + data[k]
                 print(line)
             n -= 1
 
@@ -54,8 +54,49 @@ class rectangle(AbstractPrinter):
     def __init__(self, icon):
         super().__init__(icon)
     
-    def print(self, data, level = 0):
-        pass
+    def print(self, data, level = 0, l = [], bottom = True):
+        first = False
+        if level == 0:
+            first = True
+        if data == {}:
+            return
+        pre = ""
+        pre_list = copy.copy(l)
+        for i in pre_list:
+            pre += i
+        n = len(data.keys())
+        pre_list.append("│")
+        for k in data.keys():
+            line = pre + "├"
+            if type(data[k]) == dict:
+                line += self.icon["intermediate"]
+                line += k
+            else:
+                line += self.icon["leaf"]
+                line += k
+                if data[k] != None:
+                    line += ": " + data[k]
+            for i in range(len(line), 40):
+                line += "─"
+            line += "┤"
+            if first:
+                for i in range(len(line)):
+                    if line[i] == "├":
+                        line = line[0:i] + "┌" + line[i+1:]
+                    elif line[i] == "┤":
+                        line = line[0:i] + "┐" + line[i+1:]
+                first = False
+            if bottom and (n == 1) and type(data[k]) != dict:
+                line = "└" + line[1:]
+                for i in range(len(line)):
+                    if line[i] == "├" or line[i] == "│":
+                        line = line[0:i] + "┴" + line[i+1:]
+                    elif line[i] == "┤":
+                        line = line[0:i] + "┘" + line[i+1:]
+            print(line)
+            if type(data[k]) == dict:
+                self.print(data[k], level + 1, pre_list, (n == 1) and bottom)
+            n -= 1
 
 class Explorer:
     def __init__(self):
